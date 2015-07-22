0629a309190982596b0a14887945924ee599b165
Christopher Su <christophersu9@gmail.com>
2015-07-21 23:06:53 -0700

    backup

--- routes.py
+++ routes.py
@@ -1,7 +1,3 @@
-import datetime
-import json
-import tempfile
-
 from flask import Blueprint, jsonify
 import requests
 
@@ -11,19 +7,36 @@ import secrets
 
 module = Blueprint(config['module_name'], __name__)
 
+def merge_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
 @module.route('/backup')
 @module.route('/backup/')
 @require_secret
 def backup_completed_tasks():
+    base_url = 'https://todoist.com/API/v6/get_all_completed_items?token=%s' % secrets.TODOIST_AUTH_TOKEN
 
+    projects = {}
+    items = []
 
-    temp_file = tempfile.NamedTemporaryFile()
-    temp_file.write(json.dumps(checkins))
+    limit = 50
+    offset = 0
+    while (offset == 0) or result['items']:
+        url = '%s&limit=%s&offset=%s' % (base_url, limit, offset)
+        result = requests.get(url).json()
 
-    filename = 'foursquare-%s.json' % datetime.date.today()
-    folder = secrets.BACKUP_FOLDER_ID
-    uploader.upload(temp_file.name, title=filename, parent=folder)
+        items += result['items']
+        projects = merge_dicts(projects, result['projects'])
 
-    temp_file.close()
+        offset += limit
 
-    return jsonify({'status': 'ok'})
+    uploader.quick_upload({'items': items, 'projects': projects},
+        file_prefix='todoist', folder=secrets.BACKUP_FOLDER_ID)
+
+    return jsonify({
+        'status': 'ok',
+        'items': len(items),
+        'projects': len(projects)
+    })
