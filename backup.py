c039d77653e2c0cebe43e8f2c61ba1785ec71b8b
Christopher Su <christophersu9@gmail.com>
2016-04-16 17:37:19 -0700

    backup

---
+++ backup.py
@@ -0,0 +1,33 @@
+import secrets
+import requests
+
+def merge_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+def backup_completed_tasks(uploader):
+    base_url = 'https://todoist.com/API/v6/get_all_completed_items?token=%s' % secrets.TODOIST_AUTH_TOKEN
+
+    projects = {}
+    items = []
+
+    limit = 50
+    offset = 0
+    while (offset == 0) or result['items']:
+        # TODO: refactor this to use get_completed
+        # fairly simple, but I don't want to risk breaking things for now
+        url = '%s&limit=%s&offset=%s' % (base_url, limit, offset)
+        result = requests.get(url).json()
+        items += result['items']
+        projects = merge_dicts(projects, result['projects'])
+        offset += limit
+
+    uploader.quick_upload({'items': items, 'projects': projects},
+        file_prefix='todoist', folder=secrets.BACKUP_FOLDER_ID)
+
+    return {
+        'status': 'ok',
+        'items': len(items),
+        'projects': len(projects)
+    }
--- routes.py
+++ routes.py
@@ -4,16 +4,12 @@ import arrow
 from flask import Blueprint, jsonify, request
 import requests
 
+from backup import backup_completed_tasks
 from common import require_secret, dashboard_item
 from config import config
 import secrets
 
 module = Blueprint(config['module_name'], __name__)
-
-def merge_dicts(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
 
 def get_number_tasks_completed_today():
     url = 'http://api.todoist.com/API/getProductivityStats?token=%s' % secrets.TODOIST_AUTH_TOKEN
@@ -53,30 +49,7 @@ def first_where(tasks, query):
 @module.route('/backup/')
 @require_secret
 def backup_completed_tasks():
-    base_url = 'https://todoist.com/API/v6/get_all_completed_items?token=%s' % secrets.TODOIST_AUTH_TOKEN
-
-    projects = {}
-    items = []
-
-    limit = 50
-    offset = 0
-    while (offset == 0) or result['items']:
-        # TODO: refactor this to use get_completed
-        # fairly simple, but I don't want to risk breaking things for now
-        url = '%s&limit=%s&offset=%s' % (base_url, limit, offset)
-        result = requests.get(url).json()
-        items += result['items']
-        projects = merge_dicts(projects, result['projects'])
-        offset += limit
-
-    uploader.quick_upload({'items': items, 'projects': projects},
-        file_prefix='todoist', folder=secrets.BACKUP_FOLDER_ID)
-
-    return jsonify({
-        'status': 'ok',
-        'items': len(items),
-        'projects': len(projects)
-    })
+    return jsonify(backup_completed_tasks(uploader))
 
 @module.route('/today/dashboard')
 @module.route('/today/dashboard/')
